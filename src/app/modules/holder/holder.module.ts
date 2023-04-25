15254185f768bf052bba28b0c00505e88909a136
siv <slaimtrade@gmail.com>
2023-04-25 14:33:03 +0800

    add card page layout

--- src/app/modules/holder/holder.module.ts
+++ src/app/modules/holder/holder.module.ts
@@ -16,6 +16,9 @@ import { CardFormComponent } from './card-form/card-form.component';
 import {RouterModule} from "@angular/router";
 import { CardModalComponent } from './card-modal/card-modal.component';
 import {MatDialog, MatDialogModule, MatDialogRef} from "@angular/material/dialog";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
 
 @NgModule({
   declarations: [
@@ -26,19 +29,22 @@ import {MatDialog, MatDialogModule, MatDialogRef} from "@angular/material/dialog
   exports: [
     CardsListComponent
   ],
-    imports: [
-        CommonModule,
-        StoreModule.forFeature(stateFeatureKey, cardsReducer, {metaReducers}),
-        EffectsModule.forFeature([HolderStoreEffects]),
-        MatCardModule,
-        MatButtonToggleModule,
-        MatSlideToggleModule,
-        FlexLayoutModule,
-        MatGridListModule,
-        MatButtonModule,
-        MatIconModule,
-        RouterModule,
-        MatDialogModule,
-    ]
+  imports: [
+    CommonModule,
+    StoreModule.forFeature(stateFeatureKey, cardsReducer, {metaReducers}),
+    EffectsModule.forFeature([HolderStoreEffects]),
+    MatCardModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    FlexLayoutModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+  ]
 })
 export class HolderModule { }
