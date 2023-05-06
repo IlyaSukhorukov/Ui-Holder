7b2093c6e978a97fcc01985dc948247d520c7502
siv <slaimtrade@gmail.com>
2023-05-07 02:16:01 +0800

    new card-form complete

--- src/app/modules/holder/card-form/card-form.component.ts
+++ src/app/modules/holder/card-form/card-form.component.ts
@@ -6,7 +6,7 @@ import {Store} from "@ngrx/store";
 import {loadCard} from "../store/holder-store.actions";
 import {Card} from "../store/schema";
 import {selectCard} from "../store/holder-store.selectors";
-import {FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-card-form',
@@ -23,20 +23,20 @@ export class CardFormComponent implements OnInit, OnDestroy {
 
   constructor(private _route: ActivatedRoute, private _store: Store, private _fb: FormBuilder) {
     this.formGroup = this._fb.group({...new Card});
+    this._setFormGroup();
   }
 
-  /*private static _fb(): FormGroup {
-    return new FormGroup<any>({
-      name: new FormControl(null),
-      code: new FormControl(null),
-      type: new FormControl(null),
-      access: new FormControl(null),
-    });
-  }*/
+  private _setFormGroup(): void {
+    this.formGroup.get("name")?.setValidators(Validators.required);
+    this.formGroup.get("code")?.setValidators(Validators.required);
+    this.formGroup.patchValue({access: 'private'});
+    this.formGroup.patchValue({type: 'other'});
+  }
 
   ngOnInit(): void {
     this.subscribeOnCard();
     this.subscribeOnParam();
+    this.subscribeOnParamChange(); // TODO check не работает
   }
 
   subscribeOnParam(): void {
@@ -57,8 +57,34 @@ export class CardFormComponent implements OnInit, OnDestroy {
     })
   }
 
+  subscribeOnParamChange(): void {
+    this.formGroup.valueChanges.pipe(takeUntil(this._unsubscribe$)).subscribe(($event) => {
+      // this.formGroup.markAllAsTouched();
+    });
+  }
+
   onCreate(): void {
-    console.log(this.formGroup.getRawValue());
+    // TODO validate
+    if (this.formGroup.valid) {
+      if (isNil(this.formGroup.getRawValue().type)) {
+        this.formGroup.patchValue({type: 'other'});
+      }
+      if (isNil(this.formGroup.getRawValue().access)) {
+        this.formGroup.patchValue({access: 'private'});
+      }
+      console.log(this.formGroup.getRawValue());
+    } else {
+      this.formGroup.markAllAsTouched();
+    }
+  }
+
+  isShowScanIcon(): boolean {
+    let result = false;
+    const type = this.formGroup.getRawValue().type;
+    if (type === 'qr' || type === 'bc'){
+      result = true;
+    }
+    return result;
   }
 
   ngOnDestroy(): void {
